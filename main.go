ffcd04bc0007c165a22a505a70d3f657b816b530
FF <f+git@fida.biz>
2018-12-10 12:35:52 +0000

    bg job

--- main.go
+++ main.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
+)
+
+const (
+	on  string = "on"
+	off string = "off"
 )
 
 var power string
 
 func newBatteryLevel(level int) {
 	if level >= 80 {
-		power = "off"
+		power = off
 	}
 	if level <= 75 {
-		power = "on"
+		power = on
 	}
 	log.Printf("batt=%v,power=%v\n", level, power)
 }
@@ -36,5 +46,40 @@ func update(w http.ResponseWriter, r *http.Request) {
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/update", update).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	power = on
+	bg()
+	log.Fatal(http.ListenAndServe(getEnv("ADDR", ":8000"), router))
+}
+
+func bg() {
+	var interval, err = time.ParseDuration(getEnv("INTERVAL", "10s"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	time.AfterFunc(interval, bg)
+	if power == on {
+		execEnv("POWER_ON_CMD", "echo 0x1 > /sys/devices/platform/bcm2708_usb/buspower")
+	}
+	if power == off {
+		execEnv("POWER_ON_CMD", "echo 0x0 > /sys/devices/platform/bcm2708_usb/buspower")
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func execEnv(key, fallback string) {
+	var cmd = getEnv(key, fallback)
+	var out, err = exec.Command("sh", "-c", cmd).CombinedOutput()
+	if err != nil {
+		log.Printf("cmd='%v', err='%v'", cmd, err)
+	}
+	if len(out) > 0 {
+		var outp = fmt.Sprintf("%s\n", out)
+		log.Println(strings.Trim(outp, "\n "))
+	}
 }
